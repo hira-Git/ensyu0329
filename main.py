747ed49e53fa9d5f31c9146cb3a401ea19ea48a5
hira-Git <d.hirasawa.sys20@morijyobi.ac.jp>
2021-03-29 10:57:45 +0900

    bmi計算アプリの作成

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ import calc_bmi
 
 print("BMI計算アプリ")
 w = int(input("体重(kg)を入力"))
-h = float(input("高さ(m)を入力"))
-for i in range(0,2):
-    print(f"あなたのBMIは{calc_bmi.get_bmi(w,h)}")
+h = float(input("身長(m)を入力"))
+print(f"あなたのBMIは{calc_bmi.get_bmi(w,h)}")
